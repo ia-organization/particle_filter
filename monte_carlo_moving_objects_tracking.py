baab2f97b43b7c7b44f97ac124f849efdff1b99c
Jonathan Ribeiro da Silva <jota.ribeiro.silva@gmail.com>
2019-06-29 10:38:14 -0300

    Cálculo do peso das particulas e measurement_model

--- monte_carlo_moving_objects_tracking.py
+++ monte_carlo_moving_objects_tracking.py
@@ -9,7 +9,7 @@ v_max = 25.0            # velocidade máxima
 M_PI = math.pi
 
 def normalize_theta(theta):
-    int multiplier
+    multiplier = 0
     if (theta >= -M_PI and theta < M_PI):
        return theta
    
@@ -24,37 +24,53 @@ def normalize_theta(theta):
 
 
 class Particle:
-    def __init__(self, x, y, theta, velocity, dist = None, norm_dist = None, timestamp = None):
+    def __init__(self, x, y, theta, velocity_x, velocity_y, dist = None, norm_dist = None, timestamp = None):
         self.x = x
         self.y = y
         self.theta = theta
-        self.velocity = velocity
+        self.velocity_x = velocity_x
+        self.velocity_y = velocity_y
         self.dist = dist
         self.norm_dist = norm_dist
         self.timestamp = timestamp
+        self.weight
     
 
 
     def sample_motion_model(self,delta_time):
         
-        velocity = self.velocity
+        velocity_x = self.velocity_x
+        velocity_y = self.velocity_y
         x = self.x
         y = self.y
-        theta = self.theta
-        
+        theta = self.theta        
 
-        velocity = velocity + random.gauss(0.0,alpha_1)
-
-        if(velocity > v_max):
-            velocity = v_max
-        if(velocity < v_min):
-            velocity = v_min
+            
 
         theta = theta + random.gauss(0.0, alpha_2)
         theta = normalize_theta(theta)
         
-        x = x + delta_time* velocity * math.cos(theta)
-        y = y + delta_time* velocity * math.sin(theta)
+        # x = x + delta_time* velocity * math.cos(theta)
+        # y = y + delta_time* velocity * math.sin(theta)
+
+        x = x + x * velocity_x * delta_time
+        y = y + y * velocity_y * delta_time
+
+        
+        velocity_x = velocity_x + random.gauss(0.0,alpha_1)
+        velocity_y = velocity_y + random.gauss(0.0,alpha_1)
+
+        if(velocity_x > v_max):
+            velocity_x = v_max
+        if(velocity_x < v_min):
+            velocity_x = v_min
+
+        if(velocity_y > v_max):
+            velocity_y = v_max
+        if(velocity_y < v_min):
+            velocity_y = v_min
+        
+
 
         self.velocity = velocity
         self.x = self.x
@@ -62,21 +78,45 @@ class Particle:
         self.theta = normalize_theta(theta)        
 
 
-    def distance_to_the_nearest_neighbor(self,x_z_t, y_z_t):
+    def distance_to_the_nearest_neighbor(self,obj_x, obj_y):
         distance = 0.0
         d_x, d_y = 0.0
 
-        d_x = x_z_t - self.x
-        d_y = y_z_t - self.y
+        d_x = self.x - obj_x 
+        d_y = self.y - obj_y 
 
         distance = math.sqrt(math.pow(d_x,2) + math.pow(d_y,2))
 
         return distance
 
-    def calculation_particle_weight_pose_reading_model(dist):
 
-        particle_weight = 0.0
-        particle_weight = math.exp(-dist)
-        return particle_weight
+def calculation_particle_weight_pose_reading_model(dist):
+
+    particle_weight = 0.0
+    particle_weight = math.exp(-dist)
+    return particle_weight
     
+
+
+
+def measurement_model(x, y, lst_particles):
+    num_particles = lst_particles.length()
+    lst_distance = [0.0 for i in range(num_particles)]
+    sum_weights = 0.0
+
+    
+    for i in range(num_particles):
+        lst_distance[i] = lst_particles[i].distance_to_the_nearest_neighbor(x,y)
+
+    
+    for i in range(num_particles):
+        lst_particles[i].weight = calculation_particle_weight_pose_reading_model(lst_distance[i])
+        sum_weights += lst_particles[i].weight
+
+    
+    #normalização do peso
+    for i in range(num_particles):
+        lst_particles[i].weight = (lst_particles[i].weight/ sum_weights)
+
+
 
